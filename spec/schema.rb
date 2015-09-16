84fc2ea47501ef4616f064c5140137dc6fbb59b3
pierce-h <piercewharmon@gmail.com>
2015-09-16 14:50:20 -0500

    wip

---
+++ spec/schema.rb
@@ -0,0 +1,8 @@
+ActiveRecord::Schema.define version: 0 do
+  create_table :videos, force: true do |t|
+    t.string  :asset_file_name
+    t.string  :asset_content_type
+    t.integer :asset_updated_at
+    t.integer :asset_file_size
+  end
+end
